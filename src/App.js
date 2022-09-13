04c690e2fc29d71e40489a7380d94da001527928
leesol <jbkljo@naver.com>
2022-09-13 23:27:03 +0900

    feat:현재시간 표시

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+import Clock from "./Clock";
+
+function App() {
+  return <Clock />;
+  // <div>{setInterval(<Clock />, 1)};</div>;
+}
+
+export default App;
